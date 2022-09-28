de20b8c486e6dc985d04f8c06a8a5d841ee3ca81
Gustavo Martinez <114585787+gustavom9035@users.noreply.github.com>
2022-09-28 08:53:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Gustavo M."; // update the part BETWEEN the "quotes"
+let userName = "gustavom9035"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
